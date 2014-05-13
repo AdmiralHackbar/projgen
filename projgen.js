c2dc274c6f06bbad35733032250d16623a48c89c
AdmiralHackbar <the.admiral.hackbar@gmail.com>
2014-05-12 21:20:19 -0500

    First implementation

--- projgen.js
+++ projgen.js
@@ -1,10 +1,13 @@
-var noun = ['pet', 'news', 'hotel', 'travel']
-var how = ['aggregator', 'provider', 'service', 'auction']
+var adjective = ['distributed', 'open source', 'lean', 'disruptive', 'minimal']
 
+var noun = [
+    'pet', 'news', 'hotel', 'travel', 'chat', 'dating', 'restaurant', 'food', 'cleaning', 'garden', 'picture',
+    'cat picture', 'bitcoin'];
+var how = ['aggregator', 'provider', 'service', 'auction', 'app'];
 
 function page(span) {
-
+    var adjIndex = Math.floor(Math.random() * adjective.length);
     var nounIndex = Math.floor(Math.random() * noun.length);
     var howIndex = Math.floor(Math.random() * how.length);
-
+    $('#' + span).text(adjective[adjIndex] + ' ' + noun[nounIndex] + ' ' + how[howIndex]);
 };
